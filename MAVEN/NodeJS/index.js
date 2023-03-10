de202b2e59b690c55311d517c998203af236636d
Ajit Ghimire <1903133@ritindia.edu>
2023-03-10 21:45:56 +0530

    commit

---
+++ MAVEN/NodeJS/index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+var port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
